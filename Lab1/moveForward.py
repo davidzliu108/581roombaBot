04bb5a27716eac96902c8816eb474122ad6e49ad
irion@live.unc.edu <irion@live.unc.edu>
2022-02-18 14:26:36 -0500

    fixed type in constants.py

--- Lab1/moveForward.py
+++ Lab1/moveForward.py
@@ -1,10 +1,11 @@
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+from pybricks.hubs import EV3Brick #
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, 
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from constants import LARGE_WHEEL_DIAMETER_MM
 
 def move(distanceInCM, topSpeed): 
     ev3 = EV3Brick()
