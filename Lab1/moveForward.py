ca0f007451d8244abc42d63cc59086acf74416f7
irion@live.unc.edu <irion@live.unc.edu>
2022-02-18 15:11:55 -0500

    Think I fixed the failed import problem

--- Lab1/moveForward.py
+++ Lab1/moveForward.py
@@ -1,4 +1,4 @@
-from pybricks.hubs import EV3Brick #
+from pybricks.hubs import EV3Brick 
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor, 
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
@@ -9,6 +9,6 @@ from constants import LARGE_WHEEL_DIAMETER_MM
 
 def move(distanceInCM, topSpeed): 
     ev3 = EV3Brick()
-    leftMotor = Motor(port.A)
-    rightMotor = Motor(port.B)
+    leftMotor = Motor(Port.A)
+    rightMotor = Motor(Port.B)
     
