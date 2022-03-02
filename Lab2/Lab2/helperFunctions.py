6f4d43a826ad3921c60bda74fb223b26b27b14e4
irion@live.unc.edu <67257689+irion4686@users.noreply.github.com>
2022-03-02 16:40:08 -0500

    Delete Lab2/Lab2 directory

--- Lab2/Lab2/helperFunctions.py
+++
@@ -1,9 +0,0 @@
-from pybricks.hubs import EV3Brick
-
-def waitForCenterButton():
-    ev3 = EV3Brick()
-    while (True): # pauses until button pressed
-        pressed = ev3.buttons.pressed()
-        for button in pressed:
-            if (button == button.CENTER):
-                return
