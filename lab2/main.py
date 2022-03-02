e0e99bd0a583dd338072a15c7403771b0fcfb339
irion@live.unc.edu <irion@live.unc.edu>
2022-03-02 16:33:02 -0500

    Set up basic finite state machine

--- lab2/main.py
+++ lab2/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+from unittest import case
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
@@ -6,6 +7,7 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from enum import Enum
 
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
@@ -15,6 +17,28 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 
+class State(Enum):
+    start = 0 # drive forward until wall
+    startStop = 1 # after contact with wall, back up and turn 90deg (?) to right
+    forward = 2 # drive foward until complete distance, bump wall, or distance exceeds threshold (20cm?)
+    forwardBump = 3 # back up slightly? turn right
+    forwardDistance = 4 # too far from wall, stop, turn left
+    end = 5 # stop, done
 
-# Write your program here.
-ev3.speaker.beep()
+state = State.start
+while state != State.end:
+    if state == State.start:
+        print("do something")
+    elif state == State.startStop:
+        print("do something")
+    elif state == State.forward:
+        print("do something")
+    elif state == State.forwardBump:
+        print("do something")
+    elif state == State.forwardDistance:
+        print("do something")
+    elif state == State.end:
+        print("do something")
+
+
+
