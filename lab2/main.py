24d73de981d3e0d22243782eae0a9ff49c236150
irion@live.unc.edu <irion@live.unc.edu>
2022-03-06 14:12:59 -0500

    lots of changes

--- lab2/main.py
+++ lab2/main.py
@@ -30,10 +30,6 @@ class State(enum.Enum):
 state = State.start
 """
 
-distanceRemaining = 2000
-speed = 200
-watch = StopWatch()
-
 
 def resetAndStartWatch():
     watch.reset()
@@ -49,52 +45,69 @@ def start(speed):
     return
 
 def forward(speed, distanceInMM):
+    global distanceRemaining
     ev3 = EV3Brick()
     leftMotor = Motor(Port.A)
     rightMotor = Motor(Port.D)
     touchSensorFront = TouchSensor(Port.S1)
     touchSensorCorner = TouchSensor(Port.S3)
 
-    timeNeeded = getTimeToDestinationInMS(distanceInMM, speed)
-
-    leftMotor.run_time(speed, timeNeeded, Stop.COAST, False)
-    rightMotor.run_time(speed, timeNeeded, Stop.COAST, False)
+    #timeNeeded = getTimeToDestinationInMS(distanceRemaining, speed)
+    print(distanceRemaining)
+    leftMotor.run(speed)
+    rightMotor.run(speed)
     notReached = True
     resetAndStartWatch()
     while (notReached):
         distanceRemaining -= getDistanceTraveled(speed, getDeltaTime())
+        if distanceRemaining <= 0:
+            notReached = False
+            return 6
         if touchSensorFront.pressed() == True or touchSensorCorner.pressed() == True:
             notReached = False
             leftMotor.hold()
             rightMotor.hold()
-            return
+            return 3
     return
 
 def forwardDistance(bound):
     ev3 = EV3Brick()
     sonar = UltrasonicSensor(Port.S2)
 
+    global distanceRemaining
     distance = sonar.distance(False)
-
-    while (distance > bound) :
-        moveForDistance(-1 * speed, 50, True)
-        leftCorrect(speed, 90)
-        return
+    turnInPlace(speed, -35)
+    resetAndStartWatch()
+    moveForDistance(speed, distanceRemaining, False)
+    while (sonar.distance(False) > bound or distanceRemaining <= 0) :       
+        distanceRemaining -= getDistanceTraveled(speed, getDeltaTime())
+    
+    distanceRemaining -= getDistanceTraveled(speed, getDeltaTime())
+    stop()
     return
 
 def startStop():
     moveForDistance(-1 * speed, 50, True)
-    distanceRemaining -= getDistanceTraveled(speed, getTimeToDestinationInMS(50, speed))
+    global distanceRemaining
+    distanceRemaining -= getDistanceTraveled(-1 * speed, getTimeToDestinationInMS(50, speed))
     turnInPlace(speed, 90)
-    return 2
+    return
 
 def forwardBump():
     moveForDistance(-1 * speed, 50, True)
-    distanceRemaining -= getDistanceTraveled(speed, getTimeToDestinationInMS(50, speed))
-    wait(500)
+    global distanceRemaining
+    distanceRemaining -= getDistanceTraveled(-1 * speed, getTimeToDestinationInMS(50, speed))
+    wait(200)
     turnInPlace(speed, 35)
 
 state = 0
+speed = 200
+watch = StopWatch()
+distanceRemaining = 2000
+
+sonar = UltrasonicSensor(Port.S2)
+
+
 while state != 6:
     if state == 0: #david   
         # start
@@ -103,19 +116,18 @@ while state != 6:
     elif state == 1: #cody
         # startStop
         wait(200)
-        nextState = startStop()
+        startStop()
         nextState = 2
     elif state == 2: #david
         # forward
-        forward(speed, 2000)
-        nextState = 3
+        nextState = forward(speed, distanceRemaining)
     elif state == 3: #cody
         # forwardBump
         forwardBump()
         nextState = 2
     elif state == 4: #david
         # forwardDistance
-        forwardDistance(290)
+        forwardDistance(200)
         nextState = 2
     elif state == 5: #cody
         # forwardProximity == too close to wall
