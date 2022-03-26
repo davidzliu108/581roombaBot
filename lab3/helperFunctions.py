02c6def0b82c75b83b11ea9d546664a733dffc36
davidzliu108 <davidzliu@gmail.com>
2022-03-26 15:49:12 -0400

    calculateR and calculateW functions

--- lab3/helperFunctions.py
+++ lab3/helperFunctions.py
@@ -1,4 +1,11 @@
 from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+
+
+robotWidth = 136.525
+radius = 28
 
 def waitForCenterButton():
     ev3 = EV3Brick()
@@ -7,3 +14,17 @@ def waitForCenterButton():
         for button in pressed:
             if (button == button.CENTER):
                 return
+
+def calculateR():
+    ev3 = EV3Brick()
+    leftMotor = Motor(Port.A)
+    rightMotor = Motor(Port.D)
+    r = ((rightMotor.speed()*radius + leftMotor.speed()*radius)/(rightMotor.speed()*radius - leftMotor.speed()*radius)) * (robotWidth/2)
+    return r
+
+def calculateW():
+    ev3 = EV3Brick()
+    leftMotor = Motor(Port.A)
+    rightMotor = Motor(Port.D)
+    w = (rightMotor.speed()*radius - leftMotor.speed()*radius)/robotWidth
+    return w
