0df657292097b7d171a095ef37a230d5aba4bd36
davidzliu108 <davidzliu@gmail.com>
2022-03-26 15:39:58 -0400

    calculate L and radius

--- lab3/main.py
+++ lab3/main.py
@@ -14,6 +14,9 @@ from helperFunctions import waitForCenterButton
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
+# L = 136.525mm
+# radius of wheel = 28mm
+
 
 # Create your objects here.
 ev3 = EV3Brick()
