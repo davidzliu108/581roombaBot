e3fcd6f8b279aee6d07502b039d2edc9407735b2
irion@live.unc.edu <irion@live.unc.edu>
2022-03-26 15:50:58 -0400

    Wrote calculateICC and calculateHeading

--- lab3/helperFunctions.py
+++ lab3/helperFunctions.py
@@ -1,4 +1,7 @@
+from tokenize import Triple
+from typing import Tuple
 from pybricks.hubs import EV3Brick
+from math import sin, cos
 
 def waitForCenterButton():
     ev3 = EV3Brick()
@@ -7,3 +10,12 @@ def waitForCenterButton():
         for button in pressed:
             if (button == button.CENTER):
                 return
+
+def calculateR():
+    print("")
+
+def calculateICC(currPosition, heading):
+    return Tuple(currPosition.x - calculateR() * sin(heading), currPosition.y + calculateR * cos(heading))
+
+def calculateHeading(heading, deltaTime):
+    return heading + calculateW() * deltaTime
